2a0cf2ed78fe576d9bb475ebbbc5dab3f1466567
Fajar Fadhillah <arcreactor00@gmail.com>
2026-06-09 13:44:31 +0700

    create build a javascript trivia bot project

---
+++ PROJECT JAVASCRIPT/BUILD A JAVASCRIPT TRIVIA BOT/script.js
@@ -0,0 +1,24 @@
+//CREATE GREEETING
+console.log("Hello! I'm your coding fun fact guide!");
+//CREATE VARIABLE botName, botLocation and favoriteLanguage and assign the value
+let botName = "suppaBot";
+let botLocation = "Indo";
+let favoriteLanguage = "javaScript";
+
+//console introduction name and location
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+//console favorite language
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+//create variable codingFact with assign fact 1
+let codingFact = "Fun fact bot about " + favoriteLanguage + " Is easy to learn.";
+console.log(codingFact);
+
+//create variable codingFact with assign fact 2
+codingFact = "Another fun fact bot about " + favoriteLanguage + " its fun and dynamic.";
+console.log(codingFact);
+
+//create variable codingFact with assign fact 2
+codingFact = "Another fun fact bot again about " + favoriteLanguage + " its everybody can learn it.";
+console.log(codingFact);
